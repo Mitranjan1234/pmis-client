a1ae90c3b9b13a05b7be533e7206e20360e9871f
Mitranjan1234 <Mitranjan@DESKTOP-QNDHKJN>
2024-07-15 14:09:49 +0530

    mangeusers

--- src/app/pages/manage-users/manage-users.component.ts
+++ src/app/pages/manage-users/manage-users.component.ts
@@ -11,12 +11,6 @@ export interface Audience {
   status : string,
 }
 
-export interface contact_details {
-  name : string,	
-  email:string,	
-  pass:string,	
-}
-
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.component.html',
@@ -24,13 +18,10 @@ export interface contact_details {
 })
 
 export class ManageUsersComponent implements AfterViewInit {
+  usersList !: Audience[];
+  dataSource: any;
   constructor(public userService: UserService) {
-    // this.userService.getAllUsers().subscribe((response: any) => {
-    //   console.log("Load data");
-    //   res=response
-    //   //console.log(JSON.stringify(response));
-    //   //USERS_DATA.push(response)
-    //   });
+    this.fetchUserData();
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -40,69 +31,20 @@ export class ManageUsersComponent implements AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
   }
-
-  displayedColumns: string[] = ['name', 'userId','role','status','action'];
-
-  dataSource = new MatTableDataSource<Audience>(USERS_DATA);
-
-  @ViewChild(MatPaginator)
-  paginator!: MatPaginator;
-  @ViewChild(MatSort)
-  sort!: MatSort;
-  contact_details: any;
-contacts: any;
-
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
   
-}
-
-
-const USERS_DATA: Audience[] = [
-  {
-    name: "Amit",
-    username: "amit.k@gmail.com",
-    role :"Project PI",
-    status :"Active",
-   
-  },
- 
-  {
-    "name": "Mitranjan",
-    "username": "mitranjan.k@gmail.com",
-    "role" :"CWDB personnels",
-    "status" :"Active",
-   
-  },
-  {
-    "name": "Ravi",
-    "username": "ravi.k@gmail.com",
-    "role" :"Project PI",
-    "status" :"Active",
-   
-  },
-  {
-    "name": "Nilesh",
-    "username": "nilesh.k@gmail.com",
-    "role" :"CWDB personnels",
-    "status" :"Active",
-   
-  },
-  {
-    "name": "Kamlesh",
-    "username": "kamlesh.k@gmail.com",
-    "role" :"Project PI",
-    "status" :"Active",
-   
-  },
-  {
-    "name": "Sukesh",
-    "username": "sukesh.k@gmail.com",
-    "role" :"CWDB personnels",
-    "status" :"Active",
-   
+  fetchUserData (){
+    this.userService.getAllUsers().subscribe((response: any) => {
+         this.usersList=response;
+         this.dataSource = new MatTableDataSource<Audience>(this.usersList);
+         this.dataSource.paginator=this.paginator;
+         this.dataSource.sort=this.sort;
+         })
   }
-  
-]
+  displayedColumns: string[] = ['name', 'username','role','status','action'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+}
